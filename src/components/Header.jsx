b6dcf31a02450d44018b68335ae667d065b8a4a6
olcay-elt <olcay.elt@gmail.com>
2023-09-27 15:29:54 +0200

    Delete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='flex justify-between items-center mb-6'>
             <h1 className='text-3xl font-semibold'>
-                React ve Tailwind CSS Sepet Uygulamasi
+
             </h1>
 
             <div>
