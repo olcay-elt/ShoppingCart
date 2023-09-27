015319ca79c18cd010d46d923a43dc4ffdca52a2
olcay-elt <olcay.elt@gmail.com>
2023-09-27 13:20:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+
+
+function App() {
+
+
+    return (
+        <div className='container mx-auto p-4 '>
+            <Header />
+        </div>
+    )
+}
+
+export default App
