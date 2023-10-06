e2b0067f642d5b631189db479baecbbb9bfa2a75
olcay-elt <olcay.elt@gmail.com>
2023-10-06 15:09:19 +0200

    Add new code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 import { FiShoppingCart } from 'react-icons/fi'
 
 const Header = () => {
-    return (
-        <div className='flex justify-between items-center mb-6'>
-            <h1 className='text-3xl font-semibold'>
-                React ve Tailwind CSS Sepet Uygulamasi
-            </h1>
+  return (
+    <div className='flex justify-between items-center mb-6'>
+      <h1 className='text-3xl font-semibold'>
+        React ve Tailwind CSS Sepet Uygulamasi
+      </h1>
 
-            <div>
-                <FiShoppingCart className='text-2xl' />
-                <span className='bg-red-500 text-white'>0</span>
-            </div>
-        </div>
-    )
+      <div>
+        <FiShoppingCart className='text-2xl' />
+        <span className='bg-red-500 text-white'>0</span>
+      </div>
+    </div>
+  )
 }
 
-export default Header
+export default Header;
+
+
+
+
